1b39ef99525823647ff31b3af45d6572fb118db0
villinvic13 <unconfigured@null.spigotmc.org>
2021-09-26 18:09:10 +0200

    init

---
+++ Game/units.py
@@ -0,0 +1,19 @@
+
+class Unit:
+    base_hp = 100
+    speed = 0
+    body_size = 1
+    attack_range = 1
+
+    def __init__(self):
+        pass
+
+
+
+class Soldier:
+    hp = 10
+    speed = 1
+
+
+    def __init__(self):
+        pass
