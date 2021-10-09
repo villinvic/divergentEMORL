ee00e4e143df610e2936a7eab63c657118362948
villinvic13 <unconfigured@null.spigotmc.org>
2021-10-09 20:22:27 +0200

    bob

--- EMORL/plotting.py
+++ EMORL/plotting.py
@@ -6,7 +6,7 @@ def plot_stats(perf_and_uniqueness, selected, new_pop, path):
     plt.clf()
 
     selected_set = set(selected)
-    all_indexes = set(range(len(perf_and_uniqueness)))
+    all_indexes = set(range(len(perf_and_uniqueness[0])))
     not_selected = all_indexes-selected_set
 
     cases = {'new selected' : set(),
@@ -33,8 +33,8 @@ def plot_stats(perf_and_uniqueness, selected, new_pop, path):
         plt.scatter(perf_and_uniqueness[1, list(indexes)], perf_and_uniqueness[0, list(indexes)], label=case, marker='v')
 
 
-    plt.ylabel('r$\zeta_{perf}(\pi)$')
-    plt.xlabel('r$\zeta_{kl}(\pi)$')
+    plt.ylabel(r'$\zeta_{perf}(\pi)$')
+    plt.xlabel(r'$\zeta_{kl}(\pi)$')
     plt.xscale('log')
     plt.legend()
     plt.draw()
