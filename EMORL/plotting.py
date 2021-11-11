2d6c3b424e9bababe387366a89b6d92bb6742429
villinvic <vvillin@hotmail.fr>
2021-11-11 18:41:38 +0100

    bad limits

--- EMORL/plotting.py
+++ EMORL/plotting.py
@@ -38,7 +38,7 @@ def plot_perf_uniq(perf_and_uniqueness, selected, new_pop, path):
 
     plt.ylabel(r'$\zeta_{perf}(\pi)$')
     plt.xlabel(r'$\zeta_{nov}(\pi)$')
-    plt.xlim(-0.05, 1.05)
+    #plt.xlim(-0.05, 1.05)
     plt.legend()
     plt.draw()
 
