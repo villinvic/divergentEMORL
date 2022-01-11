5b7448e1d267413d93cfe9855e174f4a52957cbc
villinvic <vvillin@hotmail.fr>
2022-01-11 16:41:31 +0100

    boxing rewards

--- EMORL/plotting.py
+++ EMORL/plotting.py
@@ -43,8 +43,8 @@ def plot_perf_uniq(perf_and_uniqueness, selected, new_pop, path):
     plt.ylabel(r'$\zeta_{perf}(\pi)$')
     plt.xlabel(r'$\zeta_{nov}(\pi)$')
     #plt.xlim(-0.05, 1.05)
-    plt.ylim(-55, 55)
-    #plt.ylim(-0.05, 1.05)
+    #plt.ylim(-55, 55)
+    plt.ylim(-0.05, 1.05)
     plt.legend()
     plt.draw()
 
